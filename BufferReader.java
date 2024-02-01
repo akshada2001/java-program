239e08e10d73bb3bda53041a3a7b250b57c24ba1
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-01 09:54:34 +0530

    c

---
+++ BufferReader.java
@@ -0,0 +1,26 @@
+        import java.io.BufferedReader;
+		import java.io.InputStreamReader;
+		import java.io.IOException;
+
+		public class BufferReader {
+
+			public static void main(String[] args)throws IOException{
+				// TODO Auto-generated method stub
+				 
+				        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+				        System.out.print("Enter your name: ");
+				        String name = reader.readLine();
+				        System.out.println("Hello, " + name + "!");
+
+				        System.out.print("Enter your age: ");
+				        int age = Integer.parseInt(reader.readLine());
+				        System.out.println("You are " + age + " years old.");
+
+				        reader.close();
+			}
+
+		
+	}
+
+
