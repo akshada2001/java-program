72d6ff852514aba149d6d591dd590bfae176545b
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-05 15:57:34 +0530

    c

---
+++ SetEx.java
@@ -0,0 +1,18 @@
+import java .util.*;
+public class SetEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Set<String> s=new HashSet();
+s.add("akshu");
+s.add("sakshu");
+s.add("pragu");
+System.out.println(s);
+SortedSet<String>s1=new TreeSet();
+s1.add("akshu");
+s1.add("sakshu");
+s1.add("pragu");
+System.out.println(s1);
+	}
+
+}
