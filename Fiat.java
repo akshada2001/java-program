0fae8268402d10eaf6835e9a2095e34cf68872ce
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-15 14:48:29 +0530

    c

---
+++ Fiat.java
@@ -0,0 +1,20 @@
+// class without main function is called POJO class( 
+public class Fiat {
+	//helps to start car
+		void start()
+		{
+			System.out.println("Use key to start");
+		}
+		//helps to stop car
+		private void stop() {
+			System.out.println("Use break to stop the car");
+		}
+		//No. of seats available in car
+		protected void seats()
+		{
+			System.out.println("Driver+Rider=1+5");
+		}
+
+	
+
+}
