3011c8dc02f08f35257dac26a867799bdfef0c32
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-15 14:57:31 +0530

    c

---
+++ ConOlEx.java
@@ -0,0 +1,18 @@
+
+public class ConOlEx {
+	public ConOlEx() {
+		System.out.println("Constructor default");
+	}
+    public ConOlEx(int x)
+    {
+    	System.out.println("with 1 parameter");
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      ConOlEx ob=new ConOlEx();
+      ConOlEx ob2=new ConOlEx(5);
+	
+
+	}
+
+}
