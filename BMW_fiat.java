f953e3628f3eb6f99ec6a40866f48df4018e6d55
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-15 14:45:43 +0530

    c

---
+++ BMW_fiat.java
@@ -0,0 +1,15 @@
+
+public class BMW_fiat  extends Fiat{
+	void start() {
+		 System.out.println("Start the car");
+	 }
+	 void stop() {
+		 System.out.println("Use ABS");
+	 }
+	 void ac()
+	 {
+		 System.out.println("Installed");
+	 }
+	}
+
+
