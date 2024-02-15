0c1ef257d206fd10178ed51dbb0b5df0ef4bbe48
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-15 15:14:27 +0530

    c

---
+++ EncapEx2.java
@@ -0,0 +1,22 @@
+
+public class EncapEx2 {
+private int a,b ;
+public EncapEx2(int a,int b) {
+	super();
+	this.a = a;
+	this.b = b;
+}
+void show()
+{
+	 System.out.println(a+" "+b);
+}
+public static void main(String[] args) {
+	EncapEx2 ob=new EncapEx2(3, 8);
+	ob.show();
+}
+
+	
+
+	}
+
+	
