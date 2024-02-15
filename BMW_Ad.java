4614d54dce070f2c2e6c251f36879e8f8f9d0e83
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-15 14:41:26 +0530

    C

---
+++ BMW_Ad.java
@@ -0,0 +1,11 @@
+
+public class BMW_Ad  extends BMW_fiat {
+	void start() {
+		System.out.println("Button start");
+	}
+	void roof_Top_Open()
+	{
+		System.out.println("roof door available");
+	}
+}
+
