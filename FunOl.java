751e69869ddbf0d7886e517e6cd061c1fec6742e
akshada2001 <akshadarlawand2001@gmail.com>
2024-02-15 14:50:50 +0530

    c

---
+++ FunOl.java
@@ -0,0 +1,37 @@
+
+public class FunOl {
+	/*void add(int i)
+    {
+   	 i++;
+   	 System.out.println(i);
+    }*/
+  /*void add(int a,int b)
+    {
+   	 int c=a+b;
+   	 System.out.println(c);
+    }*/
+	void call(int a,float b)
+	{
+		System.out.println(a+" "+b);
+	}
+  int call(float x,int y)
+  {
+	   System.out.println(x+" "+y);
+	   return 0;
+  }
+  /*void add(float c)
+    {
+   	 c++;
+   	 System.out.println(c);
+    }*/
+    
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+         FunOl ob=new FunOl();
+         //ob.add(3);
+       //  ob.add(4, 5);
+	}
+
+}
+
+
